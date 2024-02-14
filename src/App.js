df92c883f6b5593bacd48ca14a01d59b72929e9b
karan parmar <135197001+karanparmar141@users.noreply.github.com>
2024-02-14 12:06:48 +0530

    cromma

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import AllRoutes from "./allroutes/AllRoutes";
+import Header from "./componets/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css'
+import Footer from "./componets/Footer";
+import Privateroute from "./allroutes/Privateroute";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <AllRoutes/>
+    <Footer/>
+    </>
   );
 }
 
